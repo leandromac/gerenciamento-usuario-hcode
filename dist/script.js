19e730aeeca1417c4b378fca57c854789a79167e
leandromac <contato@leandromacedo.com>
2020-08-19 15:05:33 -0300

    Refatoraçãão de variáveis com forEach

--- dist/script.js
+++ dist/script.js
@@ -6,3 +6,14 @@ var inputEmail = document.querySelector("#exampleInputEmail");
 var inputPassword = document.querySelector("#exampleInputPassword");
 var inputFile = document.querySelector("#exampleInputFile");
 var checkAdmin = document.querySelector("#exampleInputAdmin");
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+fields.forEach(function(field, index) {
+  if(field.name == "gender") {
+    if(field.checked) {
+      console.log("Sim");
+    }
+  } else {
+    console.log("Não")
+  }
+});

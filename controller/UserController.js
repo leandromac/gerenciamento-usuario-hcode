512b3adf4bafdb5c7963db2bbc708d9eef850bc5
leandromac <contato@leandromacedo.com>
2020-08-21 12:13:56 -0300

    Refatorando adição de linha e status administrador

--- controller/UserController.js
+++ controller/UserController.js
@@ -79,12 +79,13 @@ class UserController {
   }
 
   addLine(dataUser) {
-    this.tableEl.innerHTML = `
+    let tr = document.createElement('tr');
+    tr.innerHTML = `
       <tr>
         <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
         <td>${dataUser.email}</td>
-        <td>${dataUser.admin}</td>
+        <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
         <td>${dataUser.birth}</td>
         <td>
           <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
@@ -92,7 +93,7 @@ class UserController {
         </td>
       </tr>
     `;
-    
+    this.tableEl.appendChild(tr);
   }
 
 }

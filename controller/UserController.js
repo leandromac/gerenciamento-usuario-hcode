981039c070c3f134f544f596b9da2e441e5c26f0
leandromac <contato@leandromacedo.com>
2020-09-04 16:34:28 -0300

    Creação de localStorage

--- controller/UserController.js
+++ controller/UserController.js
@@ -150,8 +150,11 @@ class UserController {
 
   getUsersStorage() {
     let users = []
-    if(sessionStorage.getItem("users")) {
-      users = JSON.parse(sessionStorage.getItem("users"))
+    // if(sessionStorage.getItem("users")) {
+    //   users = JSON.parse(sessionStorage.getItem("users"))
+    // }
+    if(localStorage.getItem("users")) {
+      users = JSON.parse(localStorage.getItem("users"))
     }
     return users
   }
@@ -168,7 +171,8 @@ class UserController {
   insert(data) {
     let users = this.getUsersStorage()
     users.push(data)
-    sessionStorage.setItem("users", JSON.stringify(users))
+    // sessionStorage.setItem("users", JSON.stringify(users))
+    localStorage.setItem("users", JSON.stringify(users))
   }
 
   addLine(dataUser) {

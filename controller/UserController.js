0dd5be2f62c0905ab3b1cba272b474fa1eab9d63
leandromac <contato@leandromacedo.com>
2020-09-04 17:59:48 -0300

    Refatoring

--- controller/UserController.js
+++ controller/UserController.js
@@ -137,19 +137,8 @@ class UserController {
     );
   }
 
-  getUsersStorage() {
-    let users = []
-    // if(sessionStorage.getItem("users")) {
-    //   users = JSON.parse(sessionStorage.getItem("users"))
-    // }
-    if(localStorage.getItem("users")) {
-      users = JSON.parse(localStorage.getItem("users"))
-    }
-    return users
-  }
-
   selectAll() {
-    let users = this.getUsersStorage()
+    let users = User.getUsersStorage()
     users.forEach( dataUser => {
       let user = new User()
       user.loadFromJSON(dataUser)
@@ -185,6 +174,9 @@ class UserController {
   addEventsTr(tr) {
     tr.querySelector(".btn-delete").addEventListener('click', e => {
       if(confirm("Deseja realmente excluir?")) {
+        let user = new User();
+        user.loadFromJSON(JSON.parse(tr.dataset.user));
+        user.delete();
         tr.remove();
         this.updateCount();
       }
